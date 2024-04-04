7723884cbfe49ad6496bfbf81b8b0f637ba5e9dd
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-04 09:52:15 -0600

    Add Typer as a dependency and add basic typer app

---
+++ app.py
@@ -0,0 +1,13 @@
+import typer
+from eco_sys_sim.environment import Environment
+
+environment = Environment(rows=5, cols=10)
+
+def main(
+        name: str = typer.Option(prompt="Please enter your name", help="Your legal name"),
+    ):
+    print(f"Hello {name}!")
+
+
+if __name__ == "__main__":
+    typer.run(main)
