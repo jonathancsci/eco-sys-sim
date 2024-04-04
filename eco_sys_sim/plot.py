ae098da4f23899ea0d3b7f0ba104b6fec915a6f5
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-04 15:18:52 -0600

    Change data structures for plotting

--- eco_sys_sim/plot.py
+++ eco_sys_sim/plot.py
@@ -1,13 +1,11 @@
-import numpy as np
 from matplotlib.axes import Axes
-import matplotlib.pyplot as plt
-
+from collections import deque
 
 class Plot:
     def __init__(self, ax: Axes):
         self._ax: Axes = ax
-        self._x_list = list()
-        self._y_lists = list()
+        self._x_iters: deque = deque()
+        self._y_populations: dict[deque] = dict()
 
     def update(self):
         pass
