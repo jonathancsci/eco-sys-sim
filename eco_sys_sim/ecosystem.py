669dc81322886a9d741c48dbcd42c894e1d5fe02
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-04 16:38:38 -0600

    Change plotting data structures

--- eco_sys_sim/ecosystem.py
+++ eco_sys_sim/ecosystem.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 class Ecosystem:
     def __init__(self):
+        self._animals_list: list[str] = ['bear', 'wolf', 'fox', 'deer', 'rabbit']
         self._fig, self._ax = plt.subplots()
-        self._plot = Plot(self._ax)
+        self._plot = Plot(ax=self._ax, animals_list=self._animals_list)
         self._environment = Environment(rows=5, cols=10)
--- eco_sys_sim/plot.py
+++ eco_sys_sim/plot.py
@@ -2,10 +2,12 @@ from matplotlib.axes import Axes
 from collections import deque
 
 class Plot:
-    def __init__(self, ax: Axes):
+    def __init__(self, ax: Axes, animals_list: list[str]):
         self._ax: Axes = ax
-        self._x_iters: deque = deque()
-        self._y_populations: dict[deque] = dict()
+        self._x_iters: deque[int] = deque()
+        self._y_populations: dict[str, deque[int]] = {
+            animal: deque() for animal in animals_list
+        }
 
     def update(self):
         pass
