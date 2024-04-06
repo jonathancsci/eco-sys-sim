8f58dea75c31be4a58f8dbfc069c6ed730f4a66b
Gamerscience10 <49415981+Gamerscience10@users.noreply.github.com>
2024-04-06 20:41:45 +0000

    added fox room scoring

--- eco_sys_sim/animals/fox.py
+++ eco_sys_sim/animals/fox.py
@@ -1,5 +1,14 @@
 from .animal import Animal
+from .animal import Rabbit
 
 class Fox(Animal):
     def __init__(self):
         self.size = 3
+    
+    def score_room(self, room):
+        for o in room.occupants:
+            if(type(o) == Rabbit):
+                self.add_room_score(o.nutritional_value,room)
+            elif(self.can_mate() and type(o) == Fox):
+                self.add_room_score(10)
+        self.add_room_score(self.dice_roll,room)
