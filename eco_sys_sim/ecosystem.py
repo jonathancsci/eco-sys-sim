ee296c3cb9bbfb62c09dc2f02d3f234a61382d5f
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-07 17:27:09 -0600

    Update run_simulation

--- eco_sys_sim/ecosystem.py
+++ eco_sys_sim/ecosystem.py
@@ -8,15 +8,16 @@ class Ecosystem:
 
         self._fig, self._ax = plt.subplots()
         self._plot = Plot(ax=self._ax, animals_list=self._animals_list)
-        
+
         self._environment = Environment(rows=rows, cols=cols, animals_list=self._animals_list)
         self._environment.attach(self._plot)
 
-    def run_simulation(self, max_iters: int = 0):
+    def run_simulation(self, max_iters: int = 0) -> str:
         for _ in range(max_iters):
             self.step()
             if not plt.get_fignums():
-                break
+                return 'window_closed'
+        return 'max_iters_reached'
 
     def step(self):
         self._environment.step()
