87cd039b5873fb4859ba709e6885a9fcdfafa293
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-09 17:01:37 -0600

    Impl tests for Plot

---
+++ tests/test_plot.py
@@ -0,0 +1,14 @@
+from random import randint
+import matplotlib.pyplot as plt
+from eco_sys_sim.plot import Plot
+
+class TestPlot:
+    @classmethod
+    def setup_class(cls):
+        cls.plot = Plot()
+        cls.animals_list: list[str] = ["bear", "wolf", "fox", "deer", "rabbit"]
+
+    def test_update(self):
+        dummy_data = {animal: randint(0, 9) for animal in self.animals_list}
+        self.plot.update(0, dummy_data)
+        plt.close('all')
