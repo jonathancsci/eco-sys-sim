14adad4faadc7b5dac9cffb2f7ef54413c0b4c1e
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-15 08:48:25 -0600

    Fix double plotting window pt2

--- eco_sys_sim/ecosystem.py
+++ eco_sys_sim/ecosystem.py
@@ -10,7 +10,8 @@ class Ecosystem:
     def __init__(self, rows: int = 5, cols: int = 10):
         self._animals_list: list[str] = ["bear", "wolf", "fox", "deer", "rabbit"]
 
-        self._plot = Plot(animals_list=self._animals_list)
+        self._ax = plt.subplots()[1]
+        self._plot = Plot(ax=self._ax, animals_list=self._animals_list)
 
         self._environment = Environment(
             rows=rows, cols=cols, animals_list=self._animals_list
