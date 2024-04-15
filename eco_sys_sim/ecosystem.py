80b0ba25c576c3b5f51167726c6d0f3137f1efad
Jonathan Wu <155753916+jonathancsci@users.noreply.github.com>
2024-04-15 11:22:54 -0600

    Integrate probability_of_obstacles

--- eco_sys_sim/ecosystem.py
+++ eco_sys_sim/ecosystem.py
@@ -7,14 +7,22 @@ from .status import Status, MaxIterReached, UserTerminated
 
 
 class Ecosystem:
-    def __init__(self, rows: int = 5, cols: int = 10):
+    def __init__(
+            self,
+            rows: int = 5,
+            cols: int = 10,
+            probability_of_obstacles: float = 0.2,
+            ):
         self._animals_list: list[str] = ["bear", "wolf", "fox", "deer", "rabbit"]
 
         self._ax = plt.subplots()[1]
         self._plot = Plot(ax=self._ax, animals_list=self._animals_list)
 
         self._environment = Environment(
-            rows=rows, cols=cols, animals_list=self._animals_list
+            rows=rows,
+            cols=cols, 
+            probability_of_obstacles=probability_of_obstacles,
+            animals_list=self._animals_list,
         )
         self._environment.attach(self._plot)
 
