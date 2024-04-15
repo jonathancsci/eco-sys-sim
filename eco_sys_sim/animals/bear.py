39917ef7632bd981339a184bfa7a9324987d6f48
Gamerscience10 <49415981+Gamerscience10@users.noreply.github.com>
2024-04-15 17:10:50 +0000

    animal behaviors completely implemented

--- eco_sys_sim/animals/bear.py
+++ eco_sys_sim/animals/bear.py
@@ -1,6 +1,10 @@
 from .animal import Animal
+from .deer import Deer
+from .wolf import Wolf
 
 
 class Bear(Animal):
     def __init__(self):
-        super().__init__(10,Animal.eat_meat)
+        super().__init__(10)
+        self.diet = Animal.eat_meat
+        self.attacks = [Deer, Wolf]
--- eco_sys_sim/animals/fox.py
+++ eco_sys_sim/animals/fox.py
@@ -1,20 +1,12 @@
 from .animal import Animal
 from .rabbit import Rabbit
+from .wolf import Wolf
+from .bear import Bear
 
 
 class Fox(Animal):
     def __init__(self):
-        super().__init__(3,Animal.eat_meat)
-
-    def score_grid(self, grid):
-        for o in grid.occupants:
-            if type(o) == Rabbit:
-                self.add_grid_score(o.nutritional_value, grid)
-            elif self.can_mate() and type(o) == Fox:
-                self.add_grid_score(10)
-
-    def check_for_fight(self, grid):
-        if(not self.is_full):
-            for o in grid.occupants:
-                if type(o) == Rabbit:
-                    return o
+        super().__init__(3)
+        self.diet = Animal.eat_meat
+        self.attacks = [Rabbit]
+        self.fears = [Wolf, Bear]
